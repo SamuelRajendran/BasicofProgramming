04a5270d3d85a09e5000c8cf3eff3f67768cfe32
SamuelRajendran <samuelsam9904@gmai.com>
2024-08-13 18:54:22 +0530

    "Feat : Scoping"

---
+++ Basic/FunctionAruguments.java
@@ -0,0 +1,18 @@
+package Basic;
+import java.util.Scanner;
+
+public class FunctionAruguments {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.print("Enter the Name :");
+        String name=in.nextLine();
+        String greet=Mygreet(name);
+        System.out.println(greet);
+    }
+
+    private static String Mygreet(String namm) {
+        String message="Hello "+namm;
+        return message;
+    }
+
+}
---
+++ Basic/ScopingFunction.java
@@ -0,0 +1,30 @@
+package Basic;
+
+public class ScopingFunction {
+    public static void main(String[] args) {
+        //here we Intialazing outside
+        int a=10;
+        int b=20;
+        //you can access here
+      //  System.out.println(a);
+        showme(a);
+        System.out.println(a);
+
+        //here I'm created a block
+        {
+           // I can use  variable that Intialze  outside of the block
+            a=100;
+            //here original value is updated
+            int c=200;
+            System.out.println(c);        //here i can use the variable c but i cant use it in outside
+        }
+        System.out.println(a);
+      //  System.out.println(c); here i'm trying to access the block of the variable but  i cant access it.
+
+    }
+    static void showme(int A){
+        A=1000;  //only i can use this on this block only
+        System.out.println(A);
+
+    }
+}
