b7a878b6e7a2f9ff5ab1f57201b01185d237d995
SamuelRajendran <samuelsam9904@gmai.com>
2024-08-15 17:16:46 +0530

    "Feat : Basic Array"

---
+++ Arrays/ArrayBasic.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class ArrayBasic {
+    public static void main(String[] args) {
+        //creating the array
+        //Basic structure of Arrays
+        int [] arrs=new int[5];
+        String [] names=new String[5];
+        arrs[0]=24;
+        names[0]="sam";
+       int [] rollNo={101,202,303};
+        System.out.println(rollNo);
+        System.out.println(names[0]);
+        System.out.println(arrs[1]);
+    }
+}
