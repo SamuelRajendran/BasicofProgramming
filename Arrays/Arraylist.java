4980290940679156ca5cdeef633a2f4d5516a8e2
SamuelRajendran <samuelsam9904@gmai.com>
2024-08-16 11:41:00 +0530

    "Feat : ArrayList"

---
+++ Arrays/Arraylist.java
@@ -0,0 +1,17 @@
+package Arrays;
+import java.util.*;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        //Arraylist is not fixed in size
+        //syntax
+        //we have lot of methode work on that
+        ArrayList<Integer> list=new ArrayList<>();
+        ArrayList<String> l=new ArrayList<>();
+        list.add(56);
+        list.add(568);
+        list.add(563);
+        list.add(567);
+        System.out.println(list);
+    }
+}
