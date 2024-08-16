13dccc2df919b8c87cbfd00ca8e23e3691049242
SamuelRajendran <samuelsam9904@gmai.com>
2024-08-16 21:38:27 +0530

    "Feat :Constructor"

---
+++ ClassAndObject/Carsclass.java
@@ -0,0 +1,16 @@
+package ClassAndObject;
+
+public class Carsclass {
+    String Model;
+    int Speed;
+    float Price;
+    String Colour;
+
+    Carsclass(String model,int Speed,float Price,String Colour){
+        this.Model=model;
+        this.Speed=Speed;
+        this.Price=Price;
+        this.Colour=Colour;
+    }
+
+}
---
+++ ClassAndObject/Mainclass.java
@@ -0,0 +1,11 @@
+package ClassAndObject;
+
+public class Mainclass {
+    public static void main(String[] args) {
+        Carsclass Audi=new Carsclass("ZX",436,2000000.98457f,"Black");
+        System.out.println(Audi.Speed);
+        System.out.println(Audi.Colour);
+        System.out.println(Audi.Model);
+        System.out.println(Audi.Price);
+    }
+}
