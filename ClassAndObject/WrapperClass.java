c1cf069b3ff5ce2106045c0e5c161d7413c0ec3f
SamuelRajendran <samuelsam9904@gmai.com>
2024-08-18 09:08:28 +0530

    "Feat :WrapperClass"

---
+++ ClassAndObject/WrapperClass.java
@@ -0,0 +1,17 @@
+package ClassAndObject;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+
+//Wrapper class are final which mean we cant modify anything
+        //final keyword is like a prevention the values so the original we can't modify it.
+        //for example see primitives
+        int a = 10;
+        // [ a.] //see her not getting anything method are properties
+        //But see here  Wrapper Class does
+
+        Integer sos = 44;
+        //sos.byteValue() we get lot of methodes why ? This the class -->class contains properties and functions.
+
+    }
+}
