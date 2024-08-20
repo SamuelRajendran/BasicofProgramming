97cb911f29d851c80896cf5ab5419155f030a8e3
SamuelRajendran <samuelsam9904@gmai.com>
2024-08-20 18:49:14 +0530

    "Feat :Access Modifier PUblic"

---
+++ AccessModifier/MainforPublic.java
@@ -0,0 +1,8 @@
+package AccessModifier;
+
+public class MainforPublic {
+    public static void main(String[] args) {
+        PublicClass pub=new PublicClass(10,"sam",145.24f);
+        System.out.println(pub.num);
+    }
+}
---
+++ AccessModifier/PublicClass.java
@@ -0,0 +1,16 @@
+package AccessModifier;
+
+public class PublicClass {
+    //we can access all over the world..
+    //This is not recommand to use.
+
+   public  int num;
+    public  String name;
+    public float marks;
+
+    public PublicClass(int num, String name, float marks) {
+        this.num = num;
+        this.name = name;
+        this.marks = marks;
+    }
+}
